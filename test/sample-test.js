edbe74adca461c47bb428263ffc437d59fb96701
Balendu Rawat <balendurawat1@gmail.com>
2022-09-01 19:24:39 +0530

    test2

--- test/sample-test.js
+++ test/sample-test.js
@@ -11,7 +11,18 @@ describe("Blog", function () {
 
     const posts = await blog.fetchPosts();
     expect(posts[0].title).to.equal("My first post")
+  })
 
+  if("Should edit a post", async function() {
+    const Blog = await ethers.getContractFactory("Blog");
+    const blog = await Blog.deploy("My blog");
+    await blog.deployed()
+
+    await blog.createPost("My Second post", "12345");
+    await blog.updatePost(1, "My Update Post", "2345", true)
+
+    posts = await blog.fetchPosts()
+    expect(posts[0].title).to.equal("My Updated Post")
 
   })
 
